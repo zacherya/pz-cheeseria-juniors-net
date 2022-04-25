7753052abab5bc1507e1f70bfad805077793fb36
Zac Adams <77133479+zacherya@users.noreply.github.com>
2022-04-25 15:32:27 +1000

    Modified cart-actions cypress test method for updated UI

--- client/e2e/cypress/integration/cart-actions.spec.js
+++ client/e2e/cypress/integration/cart-actions.spec.js
@@ -9,8 +9,20 @@ context('Cart Actions', () => {
     cy.get('[data-cy=add-to-cart-2]').click();
     cy.get('[data-cy=add-to-cart-3]').click();
 
+    //Wait for the notifications to disappear
+    cy.wait(6000);
+
     cy.get('[data-cy=view-cart-btn]').click();
-    cy.contains('2 Selected item')
+    cy.contains('2 Selected item');
+
+    cy.get('[data-cy=purchase-cart-btn]').click();
+    cy.get('[data-cy=confirm-action]').click();
+    //Primary test pass condition, if order was successful. Timespan will be 0 seconds ago
+    cy.contains('0 seconds ago');
+
+    //Additional order confirmation test
+    cy.contains('2 items');
+    cy.contains('$396.76');
 
   })
 
